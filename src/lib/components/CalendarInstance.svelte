b9f779eede79c5e46bd64c9c2d9ff4c5a10a0b53
Cristian Cassetta <cristiancassetta@gmail.com>
2023-03-09 05:58:47 +0100

    Created component Calendarinstnce which renderes a calendar

---
+++ src/lib/components/CalendarInstance.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Calendar from 'js-year-calendar';
+	import 'js-year-calendar/dist/js-year-calendar.css';
+
+	import type CalendarDataSourceElement from 'js-year-calendar/dist/interfaces/CalendarDataSourceElement';
+	import { onMount } from 'svelte';
+
+	export let dataSource: CalendarDataSourceElement[];
+
+	// function displayCalendar(element: HTMLDivElement) {
+	// 	let myCalendar = new Calendar(element);
+	// 	myCalendar.setDataSource(dataSource);
+	let handleClick;
+
+	onMount(() => {
+		const calendar = new Calendar('.calendar');
+		calendar.setDataSource(dataSource);
+
+		handleClick = () => {
+			calendar.addEvent({
+				name: 'Javascript lecture',
+				color: 'red',
+				startDate: new Date(2023, 1, 16),
+				endDate: new Date(2023, 1, 29)
+			});
+			console.log(calendar.getStartDate());
+		};
+	});
+</script>
+
+<button on:click={handleClick}>Click me</button>
+
+<div class="calendar">Calendar</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import CalendarInstance from '../lib/components/CalendarInstance.svelte';
+	import Calendar from 'js-year-calendar';
+	import 'js-year-calendar/dist/js-year-calendar.css';
+	import type CalendarOptions from 'js-year-calendar/dist/interfaces/CalendarOptions';
+
+	let dataSource = [
+		{
+			id: 0,
+			name: 'Google I/O',
+			location: 'San Francisco, CA',
+			startDate: new Date(2023, 0, 23),
+			endDate: new Date(2023, 0, 26)
+		},
+		{
+			id: 1,
+			name: 'Microsoft Convergence',
+			location: 'New Orleans, LA',
+			startDate: new Date(2023, 3, 16),
+			endDate: new Date(2023, 3, 19)
+		}
+	];
+</script>
+
+<!-- <button on:click={addEvent}>Click me</button> -->
+<CalendarInstance {dataSource} />
